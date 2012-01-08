9e74d41c5a058c0db5610cfd7c11310a08deb73b
Shadley Wentzel <shad6ster@gmai.com>
2012-01-08 17:55:57 +0200

    Updated show test for show client

--- spec/views/clients/show.html.erb_spec.rb
+++ spec/views/clients/show.html.erb_spec.rb
@@ -4,9 +4,13 @@ describe "clients/show.html.erb" do
   before(:each) do
     @client = assign(:client, stub_model(Client,
       :client_number => "C12341234",
+      :smp_rep_code => "SR12341234",
+      :smp_number => "S12341234",
       :title => "Name",
       :name => "Title",
-      :surname => "Surname"
+      :surname => "Surname",
+      :email_address => "test@gmail.com",
+        
     ))
     
     @comments = @client.comments.recent.limit(10).all
@@ -15,10 +19,12 @@ describe "clients/show.html.erb" do
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/C12341234/)
+    rendered.should match(/SR12341234/)
+    rendered.should match(/S12341234/)
     rendered.should match(/Title/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Surname/)
+    rendered.should match(/test@gmail.com/)
   end
 end
