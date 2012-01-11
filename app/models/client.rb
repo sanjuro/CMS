a4e933fc06196639502f95a149ef266df26baf7e
Shadley Wentzel <shad6ster@gmai.com>
2012-01-11 16:33:38 +0200

    Removed invalid char from client list. Added email validator to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -33,7 +33,10 @@ class Client < ActiveRecord::Base
                                   :maximum   => 20,
                                   :too_short => "must have at least %{count} characters",
                                   :too_long  => "must have at most %{count} characters"
-                                }      
+                                }    
+                                  
+  validates :email_address, :presence => false,
+                            :format => { :with => email_regex }   
                               
   belongs_to  :smp_rep, :primary_key => :smp_rep_code , :foreign_key => :smp_rep_code                                    
   
